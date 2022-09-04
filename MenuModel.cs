ec0b296feefa991e6fd1c5769613450fcdd62f96
francismsc <60035994+francismsc@users.noreply.github.com>
2022-09-04 10:21:58 +0100

    Add MenuModel

---
+++ MenuModel.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Lp2EpocaEspecial.Common
+{
+    public class MenuModel
+    {
+
+
+        public void OnShowRules()
+        {
+            ShowRules?.Invoke();
+        }
+
+        public void OnShowAuthor()
+        {
+            ShowAuthor?.Invoke();
+        }
+
+        public void OnStartGame()
+        {
+            StartGame?.Invoke();
+        }
+
+        public void OnShowMenu()
+        {
+            ShowMenu?.Invoke();
+        }
+
+        public event Action ShowRules;
+
+        public event Action StartGame;
+
+        public event Action ShowAuthor;
+
+        public event Action ShowMenu;
+    }
+
+
+}
