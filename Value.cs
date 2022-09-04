5e3f6522efbc62bce913d0aa468ac5ab019cf0fd
francismsc <60035994+francismsc@users.noreply.github.com>
2022-09-04 10:23:21 +0100

    Add enum Value

---
+++ Value.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Lp2EpocaEspecial.Common
+{
+
+    public enum Value
+    {
+        Black = 'B',
+        White = 'W',
+        None = 'O',
+        OutofGame = 'X',
+        Connection = '-'
+
+        
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Lp2EpocaEspecial.Common
+{
+    public class Vertex
+    {
+        public char Number{ get; set;}
+
+        public List<Vertex> Edges{get;set;}
+
+        public Value value;
+
+        public Vertex(char Number, Value value)
+        {
+            this.Number = Number;
+            Edges = new List<Vertex>();
+            this.value = value;
+           
+        }
+
+
+    }
+
+}
