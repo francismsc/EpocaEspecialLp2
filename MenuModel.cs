515d6820a182f9b9bd8e58a0aa47ec5536e0e7eb
francismsc <60035994+francismsc@users.noreply.github.com>
2022-09-05 13:22:51 +0100

    Updated MenuModel to have menuStates

--- MenuModel.cs
+++ MenuModel.cs
@@ -4,27 +4,57 @@ namespace Lp2EpocaEspecial.Common
 {
     public class MenuModel
     {
-
+        public int menustate = 4;
 
         public void OnShowRules()
         {
+            menustate = 2;
             ShowRules?.Invoke();
+        }
+
+        public int GetMenuState()
+        {
+            return menustate;
         }
 
         public void OnShowAuthor()
         {
+            menustate = 3;
             ShowAuthor?.Invoke();
         }
 
         public void OnStartGame()
         {
+            menustate = 1;
             StartGame?.Invoke();
         }
 
         public void OnShowMenu()
         {
+            menustate = 4;
             ShowMenu?.Invoke();
         }
+
+        public void OnMenuChange(int menuscreen)
+        {
+            switch (menuscreen)
+            {
+                case 1:
+                    StartGame?.Invoke();
+                    break;
+                case 2:
+                    ShowRules?.Invoke();
+                    break;
+                case 3:
+                    ShowAuthor?.Invoke();
+                    break;
+                case 4:
+                    ShowMenu?.Invoke();
+                    break;
+                
+            }
+        }
+
 
         public event Action ShowRules;
 
@@ -33,6 +63,8 @@ namespace Lp2EpocaEspecial.Common
         public event Action ShowAuthor;
 
         public event Action ShowMenu;
+
+
     }
 
 
